fe55bb42521b244ad4550c6224a0d2e3dfa70198
sauridnet <przemyslaw.n97@gmail.com>
2019-01-21 22:56:37 +0100

    add CSS to list and main component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,46 +1,46 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
 
-import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
-import { SelectRequiredValidatorDirective } from './shared/select-required-validator.directive';
+import {BsDatepickerModule} from 'ngx-bootstrap/datepicker';
+import {SelectRequiredValidatorDirective} from './shared/select-required-validator.directive';
 
-import { AppComponent } from './app.component';
-import { ListEmployeesComponent } from './employees/list-employee/list-employees.component';
-import { CreateEmployeeComponent } from './employees/create-employe/create-employee.component';
-import { ConfirmEqualValidatorDirective } from './shared/confirm-equal-validator.directive';
-import { EmployeeService } from './employees/service/employee/employee.service';
-import { DepartmentService } from './employees/service/department/depatrment-.service';
-import { DisplayEmployeeComponent } from './employees/display-employe/display-employee.component';
-import { CreateEmployeeCanDeactivateGuardService } from './employees/create-employe/create-employee-can-deactivate-guard.service';
-import { EmployeeDetailsComponent } from './employees/details-employee/employee-details.component';
-import { EmployeeFilterPipe } from './employees/list-employee/employee-filter.pipe';
-import { EmployeeListResolverService } from './employees/service/employee/employee-list-resolver.service';
-import { DepartmentListResolverService } from './employees/service/department/departments-list-resolver.service';
-import { PageNotFoundComponent } from './page-not-found.component';
-import { EmployeeDetailsGuardService } from './employees/details-employee/employee-details-guard.service';
-import { AccordionComponent } from './shared/accordion.component';
+import {AppComponent} from './app.component';
+import {ListEmployeesComponent} from './employees/list-employee/list-employees.component';
+import {CreateEmployeeComponent} from './employees/create-employe/create-employee.component';
+import {ConfirmEqualValidatorDirective} from './shared/confirm-equal-validator.directive';
+import {EmployeeService} from './employees/service/employee/employee.service';
+import {DepartmentService} from './employees/service/department/depatrment-.service';
+import {DisplayEmployeeComponent} from './employees/display-employe/display-employee.component';
+import {CreateEmployeeCanDeactivateGuardService} from './employees/create-employe/create-employee-can-deactivate-guard.service';
+import {EmployeeDetailsComponent} from './employees/details-employee/employee-details.component';
+import {EmployeeFilterPipe} from './employees/list-employee/employee-filter.pipe';
+import {EmployeeListResolverService} from './employees/service/employee/employee-list-resolver.service';
+import {DepartmentListResolverService} from './employees/service/department/departments-list-resolver.service';
+import {PageNotFoundComponent} from './page-not-found.component';
+import {EmployeeDetailsGuardService} from './employees/details-employee/employee-details-guard.service';
+import {AccordionComponent} from './shared/accordion.component';
 
 const appRoutes: Routes = [
   {
     path: 'list',
     component: ListEmployeesComponent,
-    resolve: { employeeList: EmployeeListResolverService }
+    resolve: {employeeList: EmployeeListResolverService}
   },
   {
     path: 'edit/:id',
     component: CreateEmployeeComponent,
     canDeactivate: [CreateEmployeeCanDeactivateGuardService],
-    resolve: { departmentList: DepartmentListResolverService }
+    resolve: {departmentList: DepartmentListResolverService}
   },
   {
     path: 'employees/:id', component: EmployeeDetailsComponent,
     canActivate: [EmployeeDetailsGuardService]
   },
-  { path: '', redirectTo: '/list', pathMatch: 'full' },
-  { path: 'notfound', component: PageNotFoundComponent },
+  {path: '', redirectTo: '/list', pathMatch: 'full'},
+  {path: 'notfound', component: PageNotFoundComponent},
 ];
 
 @NgModule({
@@ -67,4 +67,5 @@ const appRoutes: Routes = [
     EmployeeListResolverService, DepartmentListResolverService, EmployeeDetailsGuardService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
